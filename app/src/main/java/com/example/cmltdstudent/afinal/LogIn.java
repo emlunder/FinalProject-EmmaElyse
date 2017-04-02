1ee4404aad8dfe3f21401e34aad40033b2da4aee
emlunder <eml2210@tc.columbia.edu>
2017-04-02 17:44:15 -0400

    Updated code

---
+++ app/src/main/java/com/example/cmltdstudent/afinal/LogIn.java
@@ -0,0 +1,8 @@
+package com.example.cmltdstudent.afinal;
+
+/**
+ * Created by cmltdstudent on 4/2/17.
+ */
+
+public class LogIn {
+}
--- app/src/main/java/com/example/cmltdstudent/afinal/MainActivity.java
+++ app/src/main/java/com/example/cmltdstudent/afinal/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.cmltdstudent.afinal;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -22,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
         loginUN = (EditText) findViewById(R.id.username_fillin);
         loginPW = (EditText) findViewById(R.id.password_fillin);
         playGame = (Button) findViewById(R.id.play_button);
+
 
     }
 
