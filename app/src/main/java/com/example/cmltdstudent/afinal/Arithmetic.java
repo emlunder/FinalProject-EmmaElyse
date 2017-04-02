47561411834d30c38e6803920dff1bcd9aaba44d
emlunder <eml2210@tc.columbia.edu>
2017-04-02 17:51:03 -0400

    Updated code with blank Word Problems and Arithmetic class

---
+++ app/src/main/java/com/example/cmltdstudent/afinal/Arithmetic.java
@@ -0,0 +1,13 @@
+package com.example.cmltdstudent.afinal;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Arithmetic extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_arithmetic);
+    }
+}
---
+++ app/src/main/java/com/example/cmltdstudent/afinal/WordProblems.java
@@ -0,0 +1,13 @@
+package com.example.cmltdstudent.afinal;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class WordProblems extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_word_problems);
+    }
+}
