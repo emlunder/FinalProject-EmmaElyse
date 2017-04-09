48afae3f9d6ce94916d444c97a47b752fc705d40
emlunder <eml2210@tc.columbia.edu>
2017-04-09 19:43:45 -0400

    Changed login class to sign up class

--- app/src/main/java/com/example/cmltdstudent/afinal/MainActivity.java
+++ app/src/main/java/com/example/cmltdstudent/afinal/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 FirebaseUser user = firebaseAuth.getCurrentUser();
                 if (user == null)
-                    startActivity(new Intent(MainActivity.this, LogIn.class));
+                    startActivity(new Intent(MainActivity.this, SignUp.class));
             }
         };
 
--- app/src/main/java/com/example/cmltdstudent/afinal/SignUp.java
+++ app/src/main/java/com/example/cmltdstudent/afinal/SignUp.java
@@ -4,5 +4,5 @@ package com.example.cmltdstudent.afinal;
  * Created by cmltdstudent on 4/2/17.
  */
 
-public class LogIn {
+public class SignUp {
 }
