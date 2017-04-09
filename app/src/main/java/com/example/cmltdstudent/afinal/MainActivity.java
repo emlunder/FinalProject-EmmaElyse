630a0f48bf2b1289a215a0ea62b0d242ee23c8dd
emlunder <eml2210@tc.columbia.edu>
2017-04-09 19:36:12 -0400

    Linked to firebase

--- app/src/main/java/com/example/cmltdstudent/afinal/MainActivity.java
+++ app/src/main/java/com/example/cmltdstudent/afinal/MainActivity.java
@@ -2,13 +2,21 @@ package com.example.cmltdstudent.afinal;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private FirebaseAuth auth = FirebaseAuth.getInstance();
+    private FirebaseAuth.AuthStateListener authListener;
 
     private EditText loginUN;
     private EditText loginPW;
@@ -18,6 +26,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        authListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user == null)
+                    startActivity(new Intent(MainActivity.this, LogIn.class));
+            }
+        };
+
 
         loginUN = (EditText) findViewById(R.id.username_fillin);
         loginPW = (EditText) findViewById(R.id.password_fillin);
