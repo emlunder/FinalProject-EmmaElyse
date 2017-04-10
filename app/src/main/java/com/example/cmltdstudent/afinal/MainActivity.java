6967d7af9000eca36abb1065a75069d65daa4485
emlunder <eml2210@tc.columbia.edu>
2017-04-09 20:08:43 -0400

    Login with Email and PW but game not popping up

--- app/src/main/java/com/example/cmltdstudent/afinal/MainActivity.java
+++ app/src/main/java/com/example/cmltdstudent/afinal/MainActivity.java
@@ -4,12 +4,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Layout;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText loginUN;
     private EditText loginPW;
     private Button playGame;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,5 +54,44 @@ public class MainActivity extends AppCompatActivity {
     {
         Intent i = new Intent(this, CharacterScreen.class);
         startActivity(i);
+    }
+
+    public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+        Toast.makeText(MainActivity.this, dataSnapshot.getValue(MainActivity.class) + " has changed", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onChildRemoved(DataSnapshot dataSnapshot) {
+        Toast.makeText(MainActivity.this, dataSnapshot.getValue(MainActivity.class) + " is removed", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+    }
+
+    public void onCancelled(DatabaseError databaseError) {
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        auth.addAuthStateListener(authListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        auth.removeAuthStateListener(authListener);
+    }
+
+    public void addSomething(View view) {
+        FirebaseUser user = auth.getCurrentUser();
+        DatabaseReference userRef = database.getReference(user.getUid());
+        Layout infoText = null;
+        userRef.push().setValue(infoText.getText().toString());
+    }
+
+    public void signOut(View view) {
+        auth.signOut();
+
     }
 }
